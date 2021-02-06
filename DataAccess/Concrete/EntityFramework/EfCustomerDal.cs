0b75a38ac4949a9e2ce63683e171941e0a86fe17
Burak Baskın <2011404076@ogr.mehmetakif.edu.tr>
2021-02-06 23:55:53 +0300

    06.02.2021 day 9 end of class.

--- DataAccess/Concrete/EntityFramework/EfCustomerDal.cs
+++ DataAccess/Concrete/EntityFramework/EfCustomerDal.cs
@@ -1,17 +1,13 @@
 ﻿using Core.DataAccess.EntityFramework;
-using Core.Entities;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using System;
 using System.Collections.Generic;
-using System.Linq.Expressions;
 using System.Text;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class EfCategoryDal : EfEntityRepositoryBase<Category, NorthwindContext>, ICategoryDal
+    class EfCustomerDal : EfEntityRepositoryBase<Customer, NorthwindContext>, ICustomerDal
     {
-
     }
 }
-
