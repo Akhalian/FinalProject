9ced3e163684931df4f5aa4a1d06aee42db6e60f
Burak Baskın <2011404076@ogr.mehmetakif.edu.tr>
2021-03-13 21:35:17 +0300

    13.03.2021 commit 1

---
+++ WebAPI/Controllers/CategoriesController.cs
@@ -0,0 +1,31 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private ICategoryService _categoryService;
+
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            //Swagger
+            //Dependency chain --
+
+            var result = _categoryService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+
+        }
+
+
+    }
+
+
+}
